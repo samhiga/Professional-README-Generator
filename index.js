89de7126dd282089001eb2fac420378b4bf8e092
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-04-18 16:53:27 -0700

    added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'What is the project title?',
+    name: 'Title',
+  },
+  {
+    type: 'input',
+    message: 'What is your GitHub username',
+    name: 'GitHub username',
+  },
+  {
+    type: 'input',
+    message: 'what is your email address?',
+    name: 'email',
+  },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
